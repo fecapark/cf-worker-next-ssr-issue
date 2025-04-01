a68d8b212a5545dd67baf7ca72c1cafce3ecbe1e
fecapark <fecapark@gmail.com>
2025-04-01 09:34:18 +0900

    feat: 인터널 서버 에러를 클라이언트에서 보여주기

--- src/app/todo/[id]/page.tsx
+++ src/app/todo/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { handleError } from "@/utils/error";
 import ky from "ky";
 
 const getDynamicData = async (id: string) => {
@@ -7,14 +8,24 @@ const getDynamicData = async (id: string) => {
 
 const IdPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
-  const item = await getDynamicData(id);
 
-  return (
-    <div>
-      <h1>Todo Item</h1>
-      <p>{JSON.stringify(item)}</p>
-    </div>
-  );
+  try {
+    const item = await getDynamicData(id);
+    return (
+      <div>
+        <h1>Todo Item</h1>
+        <p>{JSON.stringify(item)}</p>
+      </div>
+    );
+  } catch (e: unknown) {
+    const err = await handleError(e);
+    return (
+      <div>
+        <h1>에러발생</h1>
+        <div>{JSON.stringify(err)}</div>
+      </div>
+    );
+  }
 };
 
 export default IdPage;
